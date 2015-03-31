6a30f440d08dd5fddcb079b811ce1beed43f5895
knjcode <knjcode@gmail.com>
2015-03-31 20:17:04 +0900

    Add decoder

--- app.jsx
+++ app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {encoder, ENC_ALGOS} from 'string-codec';
-var ALGOS = ENC_ALGOS.concat(['md5','sha1','sha224','sha256','sha384','sha512','rmd160']);
+import {encoder, decoder, ENC_ALGOS, DEC_ALGOS} from 'string-codec';
+ENC_ALGOS = ENC_ALGOS.concat(['md5','sha1','sha224','sha256','sha384','sha512','rmd160']);
 
-var Encoder = React.createClass({
+var Codec = React.createClass({
   render() {
     return (
       <ul style={style.list}>
-        {ALGOS.map((algo => {
+        {this.props.algos.map((algo => {
           try {
-            var output = encoder(this.props.str,algo)
+            var output = this.props.func(this.props.str,algo)
             return <li key={algo}><strong>{algo}:</strong> {output ? output : ''}</li>
           } catch(e) {
             return <li key={algo}><strong>{algo}:</strong> {e.toString()}</li>
@@ -37,21 +37,34 @@ var StringCodecWebIF = React.createClass({
           onChange={this.handleChange}
           autoFocus={true}
           value={this.state.value} />
-        <Encoder str={this.state.value} />
+        <div>
+        <div style={style.float}>
+          <div>encode text</div>
+          <Codec algos={ENC_ALGOS} func={encoder} str={this.state.value} />
+        </div>
+        <div style={style.float}>
+          <div>decode text</div>
+          <Codec algos={DEC_ALGOS} func={decoder} str={this.state.value} />
+        </div>
+        </div>
       </div>
     );
   }
 });
 
 var style = {
+  float: {
+    float: 'left',
+    width: '50%'
+  },
   text: {
     fontSize: '80%',
     width: 300
   },
   list: {
+    fontSize: '80%',
     margin: 0,
-    padding: 0,
-    listStyleType: 'none'
+    padding: 0
   }
 };
 
